29d031e59a12021513b658fe963930868a1a958c
Nguyen Bao Duy <29066552+bdtren@users.noreply.github.com>
2019-03-16 10:00:20 +0700

    Thêm slide, demo, tài liệu

---
+++ Nghien Cuu/LINQ Demo/prjDemoLINQ/prjDemoLINQ/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace prjDemoLINQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.LINQObject = new System.Windows.Forms.Button();
+            this.LINQXML = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(372, 310);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // LINQObject
+            // 
+            this.LINQObject.Location = new System.Drawing.Point(400, 34);
+            this.LINQObject.Name = "LINQObject";
+            this.LINQObject.Size = new System.Drawing.Size(177, 59);
+            this.LINQObject.TabIndex = 1;
+            this.LINQObject.Text = "LINQ to Object";
+            this.LINQObject.UseVisualStyleBackColor = true;
+            this.LINQObject.Click += new System.EventHandler(this.LINQObject_Click);
+            // 
+            // LINQXML
+            // 
+            this.LINQXML.Location = new System.Drawing.Point(400, 124);
+            this.LINQXML.Name = "LINQXML";
+            this.LINQXML.Size = new System.Drawing.Size(174, 59);
+            this.LINQXML.TabIndex = 2;
+            this.LINQXML.Text = "LINQ to XML";
+            this.LINQXML.UseVisualStyleBackColor = true;
+            this.LINQXML.Click += new System.EventHandler(this.LINQXML_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(400, 213);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(174, 59);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 311);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.LINQXML);
+            this.Controls.Add(this.LINQObject);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Object XML";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button LINQObject;
+        private System.Windows.Forms.Button LINQXML;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Nghien Cuu/LINQ Demo/prjLinqEntity/prjLinqEntity/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjLinqEntity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LINQENTITY_Click(object sender, EventArgs e)
+        {
+            //Data source
+            QLHSEntities qLHSEntities = new QLHSEntities();
+
+
+            //Query Creation
+            var ten = from hocsinh in qLHSEntities.HocSinhs
+                      select new
+                      {
+                          Ten = hocsinh.Ten
+                      };
+
+
+            //Query Execution
+            dataGridView.DataSource = ten.ToList();
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            dataGridView.DataSource = null;
+        }
+    }
+}
